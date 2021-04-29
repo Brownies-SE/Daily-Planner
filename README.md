546e259d40f94dc9849d81d74a8092323597e8a2
Josh Brown <brownjoshalan@gmail.com>
2021-04-29 09:43:10 -0600

    Added comments to the code as well as created a readMe

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Daily-Planner
+
+# Description
+
+This project was to test the understanding of HTML, bootstrap, and Jquery. This is a daily planner that changes colors for every business hour for the day. The project also uses local storage to save the users information in the text areas.
+
+# Installation
+
+There is no installation required. You can find it here: https://brownies-se.github.io/Daily-Planner/
+
+# Usage
+
+After opening the link above you can start typing in the text areas provided. After writing what you want, click the save button on the right hand side of the row you are typing in. The save buttons that correspond to each text area will save your text in the local storage. So if you refresh the page, your data will still be there.
+
+# Credits
+
+Contributors:
+Myself
+Denver University
+
+# Project Status
+
+Complete!
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Getting the current moment/date in format : dddd, MMMM Do YYYY
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 // $(".btn").on("click", function () {
@@ -6,12 +7,14 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 //   localStorage.setItem(id, text);
 // });
 
+//Creating the ids and setting them for local storage. Having to traverse the tree to find parent and siblings
 $(".btn").on("click", function () {
   var text = $(this).siblings(".id").val();
   var time = $(this).parent().attr("id");
   localStorage.setItem(time, text);
 });
 
+//getting the item back from the local storage
 $("#hour-09 .id").val(localStorage.getItem("hour-09"));
 $("#hour-10 .id").val(localStorage.getItem("hour-10"));
 $("#hour-11 .id").val(localStorage.getItem("hour-11"));
@@ -22,10 +25,13 @@ $("#hour-03 .id").val(localStorage.getItem("hour-03"));
 $("#hour-04 .id").val(localStorage.getItem("hour-04"));
 $("#hour-05 .id").val(localStorage.getItem("hour-05"));
 
+// current hour in military time
 var currentHour = moment().hour();
 
+//creating an array to run a conditional statement against
 var time = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+// run through the array and depending on its condition, do something
 for (i = 0; i < time.length; i++) {
   if (currentHour < time[i]) {
     $("#" + i).addClass("future");
